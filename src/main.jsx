734974f9dedf260163cda84668b983ae04e93d58
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-19 16:51:05 +0530

    Added react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
@@ -10,5 +11,6 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 )
