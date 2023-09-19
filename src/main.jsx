23825e4fc7140d571eb69e7b66b52433cdc5f07c
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-19 16:47:19 +0530

    configures auto import sorting for eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
